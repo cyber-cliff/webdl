37050f83a1ffb070af234bfbf84dccd948869b75
Noah Kim <noahbkim@gmail.com>
2016-12-11 23:27:15 -0500

    Added frameworking.

---
+++ download/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Downloader</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <form method="post">
+            <input type="text" name="url"/>
+            <input type="text" name="name"/>
+            <input type="text" name="format"/>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Downloader</title>
+        <link rel="stylesheet" type="text/css" href="/styles.css">
+    </head>
+    <body>
+        <a href="/download.php">Downloader</a>
+        <ul>
+            
+        </ul>
+    </body>
+</html>
